43aab24a96792483fa005d13cf862a4762a58d65
WyvernBai <wyvern.bai@gmail.com>
2012-03-12 15:32:30 +0800

    add funtion: url filter by regex

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,23 @@
 function open_tab(){
     var jtext = document.getElementById('mytextarea').value;
+    var regex = document.getElementById('regx').value;
     var mySplitResult = jtext.split(/[\s]+/);
+    var re = new RegExp(regex,"i");
 
     var j = 0;
     for(j = 0; j < mySplitResult.length; j++){
         if (mySplitResult[j].match(/^http:\/\//i) != null){
-            chrome.tabs.create({url: mySplitResult[j]});
+            if (regex != "")
+            {
+                if (mySplitResult[j].match(re) != null)
+                {
+                    chrome.tabs.create({url: mySplitResult[j]});
+                }
+            }
+            else
+            {
+                chrome.tabs.create({url: mySplitResult[j]});
+            }
         }
     }
 }
