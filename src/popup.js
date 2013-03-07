08cc20e2d2a8df67d399c845df0c10c24096f29d
wyvernbai <wyvern.bai@gmail.com>
2013-03-07 21:57:32 +0800

    change manifest version to 2

--- src/popup.js
+++ src/popup.js
@@ -3,22 +3,24 @@ function open_tab(){
     var regex = document.getElementById('regx').value;
     var mySplitResult = jtext.split(/[\s]+/);
     var re = new RegExp(regex,"i");
+    var url_process = "";
 
     var j = 0;
     for(j = 0; j < mySplitResult.length; j++){
-        if (mySplitResult[j].match(/^http:\/\//i) != null){
-            if (regex != "")
-            {
-                if (mySplitResult[j].match(re) != null)
-                {
-                    chrome.tabs.create({url: mySplitResult[j]});
-                }
-            }
-            else
-            {
-                chrome.tabs.create({url: mySplitResult[j]});
-            }
+      if (mySplitResult[j].match(/^http[s]?:\/\//i) != null)
+      {
+        if (regex != "")
+        {
+          if (mySplitResult[j].match(re) != null)
+          {
+            chrome.tabs.create({url: mySplitResult[j]});
+          }
         }
+        else
+        {
+          chrome.tabs.create({url: mySplitResult[j]});
+        }
+      }
     }
 }
 
@@ -32,3 +34,9 @@ function displaymessage()
     var jtext = document.getElementById('mytextarea').value;
     alert(jtext);
 }
+
+$(document).ready(function() {
+    $('#submit').click(function() {
+        open_tab();
+    });
+})
